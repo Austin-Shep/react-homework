5f5b67e931efc4163d7aa8a3c3645dfbcde75ea4
Austin.Shepherd <Austin.Shepherd.96@gmail.com>
2019-01-17 12:15:53 -0800

    Updates

---
+++ precache-manifest.84a113d108d5e8818ccdcc8234922772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81be3e5ad54c032afce0",
+    "url": "/react-homework/static/js/runtime~main.81be3e5a.js"
+  },
+  {
+    "revision": "fa524ae1f7dbc2a9b67d",
+    "url": "/react-homework/static/js/main.fa524ae1.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/react-homework/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "fa524ae1f7dbc2a9b67d",
+    "url": "/react-homework/static/css/main.1756e00b.chunk.css"
+  },
+  {
+    "revision": "88caf0fd1a735485860e93a8f2c0ac7b",
+    "url": "/react-homework/index.html"
+  }
+];
